38eef51799c5d6e8e976200d39774e95a04f966b
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:17:08 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_zeus.mk
+++ lmodroid_zeus.mk
@@ -8,9 +8,9 @@
 $(call inherit-product, device/xiaomi/zeus/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zeus
+PRODUCT_NAME := lmodroid_zeus
 PRODUCT_DEVICE := zeus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
